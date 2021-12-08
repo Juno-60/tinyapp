5ab8254f39fe2084efb1f004be5d5cce94d31f81
Nick Rusin <woundculture@gmail.com>
2021-12-08 00:49:17 +0000

    redirect thing. idk

--- express_server.js
+++ express_server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com",
 };
+
+function generateRandomString() {
+  Math.random().toString(36).substr(2, 6);
+}
 
 app.get('/', (req, res) => {
   res.send("Hello!");
@@ -19,6 +25,15 @@ app.get('/urls.json', (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
