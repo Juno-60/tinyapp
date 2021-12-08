7e0a7959c5d766af6af1bde9989486ddd277635b
Nick Rusin <woundculture@gmail.com>
2021-12-08 03:39:38 +0000

    day one tinyapp w/ redirects

--- express_server.js
+++ express_server.js
@@ -11,8 +11,8 @@ const urlDatabase = {
 };
 
 function generateRandomString() {
-  Math.random().toString(36).substr(2, 6);
-}
+  return Math.random().toString(36).substr(2, 6);
+};
 
 app.get('/', (req, res) => {
   res.send("Hello!");
@@ -33,13 +33,21 @@ app.get("/urls/new", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body.longURL);
+  const newString = generateRandomString();
+  urlDatabase[newString] = (req.body.longURL)
+  res.redirect(`/urls/${newString}`);         // Redirect to newly created string page.
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]; // references value of newly generated key
+  res.redirect(longURL); // redirects to that key, duh
 });
 
 app.get('/hello', (req, res) => {
